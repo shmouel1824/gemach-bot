668c8dfb8653f739d7382d93ae94d63328c3f6ad
shmouel1824 <b747800613@gmail.com>
2026-03-15 00:06:36 +0200

    Connect to PostgreSQL

--- gemach/settings.py
+++ gemach/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 
 import os
 load_dotenv()
